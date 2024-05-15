f171fc1cca0c13ca84c6122dca8eec8b09c96f2e
Jami-tsm <jameson.oliver.baker@gmail.com>
2024-05-15 17:00:54 -0400

    Working API calls?? I think I might have hit the limit, but it was working a second ago and I dint change anything

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 
 # Create your views here.
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=f743a61e7ada3bf0669ed715de4d2e6f'
-    city = 'Las Vegas'
-    city_weather = requests.get(url.format(city)).json()
-    weather = {
-        'city': city,
-        'temperature': city_weather['main']['temp'],
-        'description': city_weather['weather'][0]['description'],
-        'icon': city_weather['weather'][0]['icon']
-    }
-    context = {'weather':weather}
-    return render(request, 'weather/index.html',context)
+    base_url = ('http://api.openweathermap.org/data/2.5/weather?q=&units=imperial&appid=f743a61e7ada3bf0669ed715de4d2e6f')
+
+    cities = City.objects.all()
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = CityForm()
+
+    weather_data = []
+    for city in cities:
+        city_url = base_url.format(city.name)
+        city_weather = requests.get(city_url).json()
+        if city_weather.get('main') and city_weather.get('weather'):
+            weather = {
+                'city': city.name,
+                'temperature': city_weather['main']['temp'],
+                'description': city_weather['weather'][0]['description'],
+                'icon': city_weather['weather'][0]['icon']
+            }
+            weather_data.append(weather)
+        else:
+            city.delete()
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'weather/index.html', context)
